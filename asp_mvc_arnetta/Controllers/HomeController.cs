f0df9dc0adb1df55460cfb566e08af2bbbc364a1
arnettarahma <arnettarahma@gmail.com>
2017-02-06 22:23:13 +0700

    second

--- asp_mvc_arnetta/Controllers/HomeController.cs
+++ asp_mvc_arnetta/Controllers/HomeController.cs
@@ -1,11 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Web.Mvc;
+using asp_mvc_arnetta.Security;
 
 namespace asp_mvc_arnetta.Controllers
 {
-    public class HomeController
+    public class HomeController : Controller
     {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [Authorize]
+        public ActionResult Welcome()
+        {
+            return View();
+        }
+        [AuthorizeRoles("Admin")]
+        public ActionResult AdminOnly()
+        {
+            return View();
+        }
+        public ActionResult UnAuthorized()
+        {
+            return View();
+        }
     }
 }
